978bb8e3ef8f18bdb70fad476062bcc94ea9be2d
Diego Hernandez Moreno <diegohm1598@gmail.com>
2020-11-23 17:54:48 +0100

    recolectar datos de archivo json

--- api/test.js
+++ api/test.js
@@ -1,8 +1,18 @@
-module.exports = (req, res) => {
-  res.json({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies,
-  })
+
+
+const datos = require("./data/info.json")
+const mesascr = require("../src/mesa.js")
+const pedido = require("../src/pedido.js")
+
+const mesa = new mesascr.Mesa(datos.mesa.mesaN, datos.mesa.personas, datos.mesa.cuenta, datos.mesa.ocupada)
+
+
+for (let i=0; i < datos.pedidos.length; i++) {
+	mesa.incluirPedido(datos.pedidos[i].plato, datos.pedidos[i].tipoPlato, datos.pedidos[i].cantidad,
+	datos.pedidos[i].precio, datos.pedidos[i].ingredientesEvitar, datos.pedidos[i].comentarioOpcionalPlato, datos.pedidos[i].usuario)
 }
 
+
+module.exports = (req, res) => {
+	res.send("Los pedidos para la mesa" + mesa.getMesa() + " son: " + mesa.mostrarPedidos())
+}
