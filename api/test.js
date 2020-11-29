7c4b3cdef6f2b99595a3261a8c3ad4caf292c216
Diego Hernandez Moreno <diegohm1598@gmail.com>
2020-11-29 18:28:47 +0100

    modificado test.js [skip ci]

--- api/test.js
+++ api/test.js
@@ -6,11 +6,11 @@ const pedido = require("../src/pedido.js")
 
 const mesa = new mesascr.Mesa(datos.mesa.mesaN, datos.mesa.personas, datos.mesa.cuenta, datos.mesa.ocupada)
 
-
 for (let i=0; i < datos.pedidos.length; i++) {
 	mesa.incluirPedido(datos.pedidos[i].plato, datos.pedidos[i].tipoPlato, datos.pedidos[i].cantidad,
 	datos.pedidos[i].precio, datos.pedidos[i].ingredientesEvitar, datos.pedidos[i].comentarioOpcionalPlato, datos.pedidos[i].usuario)
 }
+
 
 module.exports = (req, res) => {
 		res.send("Los pedidos para la mesa: " + mesa.getMesa() + " son: \n" + mesa.mostrarPedidos())
