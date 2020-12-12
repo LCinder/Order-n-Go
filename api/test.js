fe674ba91cfcc1f5a607ed4ba1681a603dce979f
Diego Hernandez Moreno <diegohm1598@gmail.com>
2020-12-12 17:36:52 +0100

    cambiada salida apitest

--- api/test.js
+++ api/test.js
@@ -4,13 +4,12 @@ const datos = require("./data/info.json")
 const mesascr = require("../src/mesa.js")
 const pedido = require("../src/pedido.js")
 
-const mesa = new mesascr.Mesa(datos.mesa.mesaN, datos.mesa.personas, datos.mesa.cuenta, datos.mesa.ocupada)
+const mesa = new mesascr.Mesa(datos.mesas[0].mesaN, datos.mesas[0].personas, datos.mesas[0].cuenta, datos.mesas[0].ocupada)
 
-for (let i=0; i < datos.pedidos.length; i++) {
-	mesa.incluirPedido(datos.pedidos[i].platoId, datos.pedidos[i].cantidad,
-	datos.pedidos[i].ingredientesEvitar, datos.pedidos[i].comentarioOpcionalPlato, datos.pedidos[i].usuario)
+for (let i=0; i < datos.mesas[0].pedidos.length; i++) {
+	mesa.incluirPedido(datos.mesas[0].pedidos[i].platoId, datos.mesas[0].pedidos[i].cantidad,
+	datos.mesas[0].pedidos[i].ingredientesEvitar, datos.mesas[0].pedidos[i].comentarioOpcionalPlato, datos.mesas[0].pedidos[i].usuario)
 }
-
 
 module.exports = (req, res) => {
 		res.send("Los pedidos para la mesa: " + mesa.getMesa() + " son: \n" + mesa.mostrarPedidos())
