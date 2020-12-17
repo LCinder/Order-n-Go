40cfc73e04af2c6f92ba30fa068da8460f6f457e
Diego Hernandez Moreno <diegohm1598@gmail.com>
2020-12-17 16:25:04 +0100

    cambiando peticiones put y post [skip ci]

--- tests/apiTest.js
+++ tests/apiTest.js
@@ -29,9 +29,9 @@ describe("GET /2/1", function () {
 /******************************************************************************/
 /******************************Historia de Usuario 4 Cantidad******************/
 /******************************************************************************/
-describe("POST /2/1/cantidad/8", function () {
+describe("PUT /2/1/cantidad/8", function () {
 	it("Cambia la cantidad del pedido 1 de 1 a 8", async function () {
-			const response = await supertest.post("/2/1/cantidad/8")
+			const response = await supertest.put("/2/1/cantidad/8")
 			expect(response.status).to.eql(200)
 			expect(response.text).to.be.a("string").to.include("8")
 		});
@@ -47,9 +47,9 @@ describe("GET /2/1/cantidad/8", function () {
 /******************************************************************************/
 /******************************Historia de Usuario 4 Id************************/
 /******************************************************************************/
-describe("POST /2/1/id/8", function () {
+describe("PUT /2/1/id/8", function () {
 	it("Cambia el id del pedido 1 de 1 a 8", async function () {
-			const response = await supertest.post("/2/1/id/8")
+			const response = await supertest.put("/2/1/id/8")
 			expect(response.status).to.eql(200)
 			expect(response.text).to.be.a("string").to.include("8")
 			expect(response.text).to.be.a("string").to.not.include("Id Plato: 1")
@@ -67,9 +67,9 @@ describe("GET /2/8", function () {
 /******************************************************************************/
 /******************************Historia de Usuario 2***************************/
 /******************************************************************************/
-describe("PUT /2/nuevopedido/100/8", function () {
+describe("POST /2/nuevopedido/100/8", function () {
 	it("Crea un nuevo pedido con id 100 y cantidad 8", async function () {
-			const response = await supertest.put("/2/nuevopedido/100/8")
+			const response = await supertest.post("/2/nuevopedido/100/8")
 			expect(response.status).to.eql(200)
 			expect(response.text).to.be.a("string").to.include("100").to.include("8")
 		});
@@ -86,9 +86,9 @@ describe("GET /2/100", function () {
 /******************************************************************************/
 /******************************Historia de Usuario 5************************/
 /******************************************************************************/
-describe("POST /2/100/ingredientes/Curry,Otro", function () {
+describe("PUT /2/100/ingredientes/Curry,Otro", function () {
 	it("Incluye en el pedido 8 los ingredientes a evitar curry y otro", async function () {
-			const response = await supertest.post("/2/100/ingredientes/Curry,Otro")
+			const response = await supertest.put("/2/100/ingredientes/Curry,Otro")
 			expect(response.status).to.eql(200)
 			expect(response.text).to.be.a("string").to.include("Curry,Otro")
 		});
@@ -104,9 +104,9 @@ describe("GET /2/100", function () {
 /******************************************************************************/
 /******************************Historia de Usuario 1***************************/
 /******************************************************************************/
-describe("POST /2/usuarios/8", function () {
+describe("PUT /2/usuarios/8", function () {
 	it("Cambia el numero de personas de la mesa 2 a 8", async function () {
-			const response = await supertest.post("/2/usuarios/8")
+			const response = await supertest.put("/2/usuarios/8")
 			expect(response.status).to.eql(200)
 		});
 });
@@ -157,9 +157,9 @@ describe("GET /2/1", function () {
 /******************************************************************************/
 /******************************Historia de Usuario 8***************************/
 /******************************************************************************/
-describe("POST /2/pagarPorSeparado", function () {
+describe("PUT /2/pagarPorSeparado", function () {
 	it("Devuelve que cada usuario quiere pagar por separado que debe pagar cada usuario", async function () {
-			const response = await supertest.post("/2/pagarPorSeparado")
+			const response = await supertest.put("/2/pagarPorSeparado")
 			expect(response.status).to.eql(200)
 			expect(response.text).to.be.a("string").to.include("usuario")
 		});
@@ -175,16 +175,16 @@ describe("GET /2", function () {
 /******************************************************************************/
 /******************************Historia de Usuario 8***************************/
 /******************************************************************************/
-describe("POST /2/pagarPorSeparado", function () {
+describe("PUT /2/pagarPorSeparado", function () {
 	it("Devuelve la cantidad que debe pagar cada usuario", async function () {
-			const response = await supertest.post("/2/pagarPorSeparado")
+			const response = await supertest.put("/2/pagarPorSeparado")
 			expect(response.status).to.eql(200)
 		});
 });
 
 describe("GET /2/pagarSeparado", function () {
 	it("Obtiene pagarPorSeparado precio", async function () {
-			const response = await supertest.post("/2/pagarPorSeparado")
+			const response = await supertest.put("/2/pagarPorSeparado")
 			expect(response.status).to.eql(200)
 			expect(response.text).to.be.a("string").to.include("30")
 		});
