49a578ddd892b39d80e2756bee00ebee72e6104b
Diego Hernandez Moreno <diegohm1598@gmail.com>
2021-01-18 18:02:15 +0100

    cambiada ruta en test de la API

--- tests/apiTest.js
+++ tests/apiTest.js
@@ -1,5 +1,5 @@
 
-const supertest = require("supertest")("0.0.0.0:5000")
+const supertest = require("supertest")("https://orderngo.herokuapp.com")
 const fastify = require("./../src/routes.js")
 const expect = require("chai").expect
 
